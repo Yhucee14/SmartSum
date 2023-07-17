566de7717788d902f11cac06b772439cb5b09cd2
Yhucee14 <uchechukwu7814@gmail.com>
2023-07-17 08:35:23 -0700

    updated the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
     <h1 className='head_text'>
       Summarize Articles using <br className="max-md:hidden"/>
-      <span className='purple_gradient'>OpenAi GPT-4</span>
+      <span className='purple_gradient'>OpenAi GPT</span>
     </h1>
     <h1 className="desc">
       Simplify your reading with <b>SmartSum</b>, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
