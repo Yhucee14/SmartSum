a225eb805bf417998a08e61bb775a9e992de38bb
Yhucee14 <uchechukwu7814@gmail.com>
2023-07-22 09:02:45 -0700

    finished Article summarizer

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,7 @@
-import {useState, useEffect} from 'react'
-import {copy, linkIcon, loader, tick} from '../assets'
+import { useState, useEffect } from 'react'
+import { copy, linkIcon, loader, tick } from '../assets'
 import { useLazyGetSummaryQuery } from '../services/article';
+import '../App.css'
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -8,65 +9,113 @@ const Demo = () => {
     summary: '',
   });
 
-const [allArticles, setAllArticles] = useState([]);
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
 
-const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-useEffect(() => {
-  const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles')
-  )
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles')
+    )
 
-if (articlesFromLocalStorage) {
-  setAllArticles(articlesFromLocalStorage)
-}
-}, []);
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, []);
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const {data} = await getSummary({articleUrl: article.url});
+    const { data } = await getSummary({ articleUrl: article.url });
 
-  if(data?.summary){
-    const newArticle = {...article, summary: data.summary};
-    const updatedAllArticles = [newArticle, ...allArticles];
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
 
-    setArticle(newArticle);
-    setAllArticles(updatedAllArticles);
-    
-    localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+    }
   }
-}
-  
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  }
+
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
       <div className='flex flex-col w-full gap-2'>
         <form
-        className='relative flex justify-center '
-        onSubmit={handleSubmit}>
+          className='relative flex justify-center '
+          onSubmit={handleSubmit}>
           <img src={linkIcon} alt='link_icon'
-          className='absolute left-0 top-1 my-2 ml-3 w-5'/>
+            className='absolute left-0 top-1 my-2 ml-3 w-5' />
 
-      <input 
-        type='url'
-        placeholder='Enter a URL'
-        value={article.url}
-        onChange={(e) => setArticle({...article, url: e.target.value})} required 
-        className='url_input peer'/>
+          <input
+            type='url'
+            placeholder='Enter a URL'
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })} required
+            className='url_input peer' />
 
-        <button
-        type='submit'
-        className='submit_btn peer-focus:border-gray-700 peer-focustext-gray-700'>
-          go
-        </button>
-        </form>
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focustext-gray-700'>
+            go
+          </button>
+        </form> <br />
 
-      {/* Browse URL History*/}
+        {/* Browse URL History*/}
+        <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+          Article <span className='blue_gradient'>History</span>
+        </h2>
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.map((item, index) => (
+            <div key={`link-${index}`} onClick={() => setArticle(item)}
+              className='link_card'>
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+                <img src={copied === item.url ? tick : copy} alt='copy_icon'
+                  className='w-[40%] h-[40%] object-contain' />
+              </div>
+              <p className='flex- font-satoshi text-blue-800 font-medium text-sm truncate'>
+                {item.url}
+              </p>
 
-       
+            </div>
+          ))}
+        </div>
+
       </div>
 
       {/* Display Result */}
+
+      <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (<img src={loader} alt='loader' className='w-20 h-20 object-contain' />) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className='font-satoshi font-normal text-gray-600'>
+              {error?.data.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )
+        }
+      </div>
     </section>
   )
 }
